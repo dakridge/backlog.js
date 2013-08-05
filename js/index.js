d666ea7e725d5a47a328c343fefee4fb5a86dafa
Doug Akridge <doug.a.akridge@gmail.com>
2013-08-04 21:14:11 -0400

    add a sorting function
    adds a sorting function to the labels

--- js/index.js
+++ js/index.js
@@ -1,7 +1,33 @@
 (function()
 {
+	backlog = [];
+
 	var p = getQueryParams(window.location.search);
 	var url = (p.hasOwnProperty("key")) ? "https://docs.google.com/spreadsheet/pub?key=" + p["key"] + "&single=true&gid=0&output=csv" : false;
+
+	var priorities = {
+			"h"	: "High",
+			"m"	: "Medium",
+			"l"	: "Low"
+		};
+
+	$("#backlog-labels").on("click", ".label", function(e)
+	{
+		var that = $(this);
+
+		if( that.hasClass("type") )
+		{
+			backlog.sortByKey("Type of Request");
+		}
+		if( that.hasClass("uid") )
+		{
+			backlog.sortByKey("ID");
+		}
+
+		$("#backlog-list").empty();
+		build(backlog);
+
+	});
 
 	var list = d3.select("#backlog-list");
 
@@ -11,25 +37,9 @@
 		{
 			console.log( res );
 
-			// DATA JOIN
-			// Bind data to the list
-			var items = list.selectAll(".item")
-				.data( res );
+			backlog = res;
 
-			// ENTER
-			// Create new elements as needed.
-			var item = items.enter()
-				.append("div")
-				.attr("class", "item");
-
-			item.append("div")
-				.attr("class", function(b){ return "type " +  b["Type of Request"].toLowerCase().replace(/\s+/g, '') })
-				.append("div")
-				.text(function(b){ return b["Type of Request"] });
-
-			item.append("div")
-				.attr("class", "description")
-				.text(function(b){ return b["Description"] });
+			build(res);
 		});
 	}
 	else
@@ -39,6 +49,38 @@
 
 		list.append("h3")
 			.text("Example: ?key=0Ar9b16u8gRNVdEtkWXo5bGhUZ3lTVnFldVhObTRIdWc");
+	}
+
+	function build(res)
+	{
+		// DATA JOIN
+		// Bind data to the list
+		var items = list.selectAll(".item")
+			.data( res );
+
+		// ENTER
+		// Create new elements as needed.
+		var item = items.enter()
+			.append("div")
+			.attr("class", "item");
+
+		item.append("div")
+			.attr("class", function(b){ return "type " +  b["Type of Request"].toLowerCase().replace(/\s+/g, '') })
+			.append("div")
+			.text(function(b){ return b["Type of Request"] });
+
+		item.append("div")
+			.attr("class", "uid")
+			.text(function(b){ return b["ID"] });
+
+		item.append("div")
+			.attr("class", "description")
+			.text(function(b){ return b["Description"] });
+
+		item.append("div")
+			.attr("title", function(b){ return ( b["Priority"] != undefined ) ? priorities[ b["Priority"].toLowerCase() ] + " Priority" : "No Priority" })
+			.attr("class", function(b){ return "priority " + (( b["Priority"] != undefined ) ? b["Priority"].toLowerCase() : "none") })
+			.append("div");
 	}
 
 })();
--- js/tools.js
+++ js/tools.js
@@ -12,3 +12,12 @@ function getQueryParams(qs)
 
 	return params;
 }
+
+Array.prototype.sortByKey = function(key)
+{
+	return this.sort(function(a, b)
+	{
+		var x = a[key]; var y = b[key];
+		return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+	});
+};
