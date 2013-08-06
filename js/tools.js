486013587987d6c5abe7a778f584cbad88158466
Doug Akridge <doug.a.akridge@gmail.com>
2013-08-06 19:42:17 -0400

    sort uses id as secondary
    the sort function uses the id to sort the item

--- js/tools.js
+++ js/tools.js
@@ -18,7 +18,9 @@ Array.prototype.sortByKey = function(key)
 	return this.sort(function(a, b)
 	{
 		var x = a["backlog"][key]; var y = b["backlog"][key];
-		return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+		var x2 = a["backlog"]["id"]; var y2 = b["backlog"]["id"];
+		
+		return ((x < y) ? -1 : ((x > y) ? 1 : (x2 < y2) ? -1 : (x2 > y2) ? 1 : 0 ));
 	});
 };
 
