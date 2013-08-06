5d6a87b97a7589f8aad9f9495e07142b322bf25e
Doug Akridge <doug.a.akridge@gmail.com>
2013-08-06 11:04:25 -0400

    rainbow, validator, hide finished
    the rainbow will automatically decide the colors of the labels within the
    javascript using a preset array of color values. Colors can easily be
    added or removed at any time.
    
    the validator makes sure each item has the expected properties.
    
    by default, backlog.js does not show finished tasks... but there is
    a button to show them if you want.

--- js/index.js
+++ js/index.js
@@ -33,7 +33,13 @@
 		backlog.sortByKey( sort );
 		$("#backlog-list").empty();
 		build(backlog);
+	});
 
+	$("#viewhidden").on("click", function(e)
+	{
+		var that = $(this);
+		that.toggleClass("showing");
+		$("#backlog-list").toggleClass("showing");
 	});
 
 	var list = d3.select("#backlog-list");
@@ -59,6 +65,9 @@
 
 	function build(res)
 	{
+		validate(res);
+		var colors = rainbow(res);
+
 		// DATA JOIN
 		// Bind data to the list
 		var items = list.selectAll(".item")
@@ -68,32 +77,104 @@
 		// Create new elements as needed.
 		var item = items.enter()
 			.append("div")
-			.attr("class", "item")
-			.each( function(b)
-			{
-				if( priorityOrder.indexOf( b["Priority"].toLowerCase() ) < 0 ){ b["Priority"] = "none"; }
-
-				b["ID"] = +b["ID"];
-
-			});
+			.attr("class", function(b){ return "item " + b.backlog.isdone });
 
 		item.append("div")
-			.attr("class", function(b){ return "type " +  b["Type of request"].toLowerCase().replace(/\s+/g, '') })
+			.attr("class", function(b){ return "type " +  b.backlog["type of request"].toLowerCase().replace(/\s+/g, '') })
 			.append("div")
-			.text(function(b){ return b["Type of request"] });
+			.style("background-color", function(b){ return colors[ b.backlog["type-class"] ] })
+			.text(function(b){ return b.backlog["type of request"] });
 
 		item.append("div")
 			.attr("class", "uid")
-			.text(function(b){ return b["ID"] });
+			.text(function(b){ return b.backlog["id"] });
 
 		item.append("div")
 			.attr("class", "description")
-			.text(function(b){ return b["Description"] });
+			.text(function(b){ return b.backlog["description"] });
 
-		item.append("div")
-			.attr("title", function(b){ return priorities[ b["Priority"].toLowerCase() ] + " Priority" })
-			.attr("class", function(b){ return "priority " + b["Priority"].toLowerCase() })
+		var type = item.append("div")
+			.attr("title", function(b){ return priorities[ b.backlog["priority"].toLowerCase() ] + " Priority" })
+			.attr("class", function(b){ return "priority " + b.backlog["priority"].toLowerCase() })
 			.append("div");
+	}
+
+	function rainbow(a)
+	{
+		var colors = ["#9b59b6", "#27ae60", "#3498db", "#e67e22", "#e74c3c", "#34495e", "#1abc9c"];
+		var types = [];
+		var count = 0;
+
+		var typeColors = {};
+
+		a.forEach(function( d, i )
+		{
+			var s = d.backlog["type of request"].toLowerCase().replace(/\s+/g, '');
+			d.backlog["type-class"] = s;
+
+			if( types.indexOf( s ) < 0 )
+			{
+				types.push( s );
+				typeColors[ s ] = colors[ count % colors.length ];
+
+				count++
+			}
+		});
+
+		return typeColors;
+	}
+
+	function validate(a)
+	{
+		//sets up the backlog array for use
+
+		//these are the required properties
+		var props = ["Priority", "Description", "Type of request", "ID"];
+
+		a.forEach(function(d, i)
+		{
+			d.backlog = {};
+
+			props.forEach(function(b)
+			{
+				var bl = b.toLowerCase();
+
+				if( !d.hasOwnProperty(b) )
+				{
+					d.backlog[ bl ] = "none";
+				}
+				else
+				{
+					d.backlog[ bl ] = d[b];
+				}
+
+				if( bl == "id" )
+				{
+					d.backlog[ bl ] = +d.backlog[ bl ];
+				}
+				else if( bl == "priority" )
+				{
+					d.backlog[ bl ] = d.backlog[ bl ].toLowerCase();
+
+					if( priorityOrder.indexOf( d.backlog[ bl ] ) < 0 )
+					{
+						d.backlog[ bl ] = "none";
+					}
+				}
+			});
+
+			if( d.hasOwnProperty("Release Date") && d["Release Date"] != undefined && d["Release Date"].length )
+			{
+				d.backlog["released"] = d["Release Date"];
+				d.backlog["isdone"] = "isdone";
+			}
+			else
+			{
+				d.backlog["released"] = "";
+				d.backlog["isdone"] = "notdone";
+			}
+
+		});
 	}
 
 })();
